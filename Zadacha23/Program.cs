fb813fbd39f926d24263151f39bc8c0f1327f7ae
masyaka.94 <masyaka.94@gmail.com>
2022-10-08 22:14:49 +0500

    Добавлена задача 23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Vvedite chislo N");
+int N = int.Parse(Console.ReadLine());
+
+for (int chislo=1;chislo<=N;chislo++)
+{
+   Console.WriteLine($"Куб числа {chislo} равен - "+ Math.Pow(chislo,3));
+}
